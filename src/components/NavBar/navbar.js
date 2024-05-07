54e52ced9d2b75bdfa9574979a47fa668929f9ea
umme-kulsum-j <ummejamali@hotmail.com>
2024-05-06 18:25:14 -0700

    slight changes to nav

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -4,13 +4,14 @@ import './navbar.css';
 export default function Navbar(props) {
     return (
         <div className="navbar-container">
+            <img className="menu-icon"src="/public/img/website-icon.png"/>
+
             <div className="navbar-links">
                 <div>Home</div>
                 <div>Find Help</div>
                 <div>Identify Abuse</div>
                 <div>Connect</div>
             </div>
-                <img className="menu-icon"src="/public/img/website-icon.png"/>
         </div>
     );
 }
