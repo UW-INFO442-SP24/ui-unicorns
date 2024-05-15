d549f1743175c9d42214119bb36adb0f01c4fa2d
ASABANOVIC26 <SABANOVIC2002@GMAIL.COM>
2024-05-15 10:01:41 -0700

    fixed nav

--- src/components/Identify/identify.js
+++ src/components/Identify/identify.js
@@ -1,5 +1,23 @@
 import React from 'react';
 
 export default function Identify(props) {
+    return (
+        <div className='FindHelp'>
+            <div className='FindHelpTitle'>
+                <h1>Identify Abuse</h1>
+            </div>
 
+            <div className='IdentifyVideo'>
+                <iframe 
+                    width="560" 
+                    height="315" 
+                    src="https://www.youtube.com/embed/_RqXZL93kyQ" 
+                    title="YouTube video player" 
+                    frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    allowfullscreen
+                ></iframe>
+            </div>
+        </div>
+    )
 }
